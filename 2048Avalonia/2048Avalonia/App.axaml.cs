dcc8f8269824d6a11eab89f3bbcfec54bc1fb2d8
Shurik01 <128497231+Shurik01@users.noreply.github.com>
2026-09-26 23:09:53 +0900

    add: add app.axaml.cs

---
+++ 2048Avalonia/2048Avalonia/App.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using _2048Avalonia.ViewModels;
+using _2048Avalonia.Views;
+
+namespace _2048Avalonia;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new StartWindow
+            {
+                DataContext = new MainViewModel(),
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
